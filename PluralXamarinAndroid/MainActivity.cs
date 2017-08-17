677bc5baba5e7dbc7b6db4413e19eef6be87d022
Anderson Fernandes <aasf86@gmail.com>
2017-08-17 19:14:51 -0300

    demo

---
+++ PluralXamarinAndroid/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace PluralXamarinAndroid
+{
+    [Activity(Label = "PluralXamarinAndroid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            FindViewById<Button>(Resource.Id.btnHellow).Click += (s, e) => 
+            {
+                Toast.MakeText(this, "Hey, you touch me, thanks!!", ToastLength.Short).Show();
+            };
+        }
+    }
+}
+
